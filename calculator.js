4dc85915a4af77569cd53a9d1273725702876ba6
rabia ölker <rabia.olker@gmail.com>
2022-01-14 18:02:15 +0300

    gif dosyası eklendi

---
+++ calculator.js
@@ -0,0 +1,11 @@
+const result=document.querySelector("#result")
+const btns=document.getElementsByTagName("button").innerHTML
+console.log(btns)
+
+function Calculator(){
+
+
+}
+for(let button of btns){
+    console.log(button)
+}
